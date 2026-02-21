e5e60b961ab98ee6c1d382434025c4e484818625
designDefined <designdefined.by@gmail.com>
2026-02-21 23:41:02 +0900

    feat: PWA 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "달토끼 책방",
+        short_name: "달토끼 책방",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+          // { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
 });
